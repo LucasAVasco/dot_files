81e69063b4871f3fa7a90c5e3d75117b2d1d5c0a
LucasAVasco <none>
2020-11-17 17:58:29 -0300

    Finix basic topbar customization
    Add new configuration to topbar.
    
    Add new colors to topbar: 'dark-blue' and 'light-blue'.

--- .config/polybar/bars/topbar/battery_check.sh
+++ .config/polybar/bars/topbar/battery_check.sh
@@ -6,11 +6,11 @@ declare -i BATTERY_NOW=$(cat /sys/class/power_supply/*/energy_now | sed -n '1p')
 declare -i BATTERY_PERC=$BATTERY_NOW*100/$BATTERY_FULL
 
 # Icons
-ICON=''
-declare -i SELECTED_ICON=${BATTERY_PERC}/10-1
+ICON=''
+declare -i SELECTED_ICON=${BATTERY_PERC}/20-1
 
 # Output
-echo " ${BATTERY_PERC} ${ICON:${SELECTED_ICON}:1}"
+echo " ${ICON:${SELECTED_ICON}:1} ${BATTERY_PERC}% "
 
 # Unset
 unset BATTERY_PERC BATTERY_FULL BATTERY_NOW ICON SELECTED_ICON
