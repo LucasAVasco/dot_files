e1e3e9aef3c0de005e0beed5f7a56fa41a94d1d2
LucasAVasco <none>
2021-02-01 18:30:48 -0300

    Add beautiful and focus padding
    Bspwm will init with beautiful padding.
    
    Sxhkd can switch between these padding.

---
+++ .config/bspwm/beautiful_padding.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+bspc config top_padding 31
+bspc config right_padding 5
+bspc config bottom_padding 5
+bspc config left_padding 5
+bspc config window_gap 10
---
+++ .config/bspwm/focus_padding.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+bspc config top_padding 26
+bspc config right_padding 0
+bspc config bottom_padding 0
+bspc config left_padding 0
+bspc config window_gap 0
