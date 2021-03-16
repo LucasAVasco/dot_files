392b1bdcfef097af591448f11a5e885c5dd34de6
LucasAVasco <none>
2021-03-16 12:05:51 -0300

    Rofi session manager
    Removed obsession. Switch to a custom Rofi session manager.

---
+++ .config/rofi/session-manager.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+# Define the CONTENT
+CONTENT=''
+
+# Get the CONTENT
+for loop in 'Shutdown;system-shutdown' 'Reboot;system-reboot' 'Suspend;system-suspend' 'Hibernate;system-hibernate' 'Lock screen;lock-screen' 'Switch user;system-switch-user'; do
+	CONTENT+=$(echo -n "$loop" | sed 's/;/\\0icon\\x1f/')
+	CONTENT+='\n'
+done
+
+# Display ROFI
+SELECTED_COMMAND=$(echo -en "$CONTENT" | rofi -dmenu -theme themes/session-manager.rasi)
+
+# Execute the command
+case $SELECTED_COMMAND in
+	# Shutdown
+	Shutdown )
+		shutdown now
+	;;
+
+	# Reboot
+	Reboot )
+		reboot
+	;;
+
+	# Suspend
+	Suspend )
+		systemctl suspend
+	;;
+
+	# Hibernate
+	Hibernate )
+		systemctl hibernate
+	;;
+
+	# Lock-screen
+	'Lock screen' )
+		slimlock 2> '/dev/null' &
+		sleep 1
+		xset dpms force off
+	;;
+
+	# Switch-user
+	'Switch user' )
+		pkill bspwm
+	;;
+
+esac
