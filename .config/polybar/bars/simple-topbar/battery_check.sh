26ecd7d20cab63e098933b2ca5e91a5560985b26
Lucas Vasco <lucas@pop-os.localdomain>
2021-03-31 13:37:42 -0300

    New polybar theme 'simple-topbar'

---
+++ .config/polybar/bars/simple-topbar/battery_check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Calculations
+declare -i BATTERY_FULL=$(cat /sys/class/power_supply/*/energy_full | sed -n '1p')
+declare -i BATTERY_NOW=$(cat /sys/class/power_supply/*/energy_now | sed -n '1p')
+declare -i BATTERY_PERC=$BATTERY_NOW*100/$BATTERY_FULL
+
+# Icons
+ICON=''
+declare -i SELECTED_ICON=${BATTERY_PERC}/20-1
+
+# Output
+echo "   ${ICON:${SELECTED_ICON}:1} ${BATTERY_PERC}% "
+
+# Unset
+unset BATTERY_PERC BATTERY_FULL BATTERY_NOW ICON SELECTED_ICON
