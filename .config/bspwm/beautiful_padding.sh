1eb7c6fd253a58d36db73cc88521e8ba47a68969
LucasAVasco <none>
2021-04-04 16:34:12 -0300

    Bspwm single monocle
    Add support to bspwm single monocle.

--- .config/bspwm/beautiful_padding.sh
+++ .config/bspwm/beautiful_padding.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# Multiple nodes
 bspc config top_padding 31
 bspc config right_padding 5
 bspc config bottom_padding 5
 bspc config left_padding 5
 bspc config window_gap 10
+
+# Single monocle
+bspc config top_monocle_padding 10
+bspc config right_monocle_padding 10
+bspc config bottom_monocle_padding 10
+bspc config left_monocle_padding 10
