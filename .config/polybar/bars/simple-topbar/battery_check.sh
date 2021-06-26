716e089a09f5e289609d264d4cd97a2620f6e4e7
LucasAVasco <none>
2021-06-26 15:46:44 -0300

    fix: battery check can't recognize some devices
    Before, the '~/.config/polybar/bars/simple-topbar/battery_check.sh' script
    can't recognize some battery devices because the '/sys/class/power_supply/BAT*/'
    directory has 'charge_full' and 'charge_now' files instead of 'energy_full' and
    'energy_now'.
    
    This bux was fixed.
    
    ---
    Changes to be committed:
          modified:   .config/polybar/bars/simple-topbar/battery_check.sh

--- .config/polybar/bars/simple-topbar/battery_check.sh
+++ .config/polybar/bars/simple-topbar/battery_check.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-# Calculations
-declare -i BATTERY_FULL=$(cat /sys/class/power_supply/*/energy_full | sed -n '1p')
-declare -i BATTERY_NOW=$(cat /sys/class/power_supply/*/energy_now | sed -n '1p')
-declare -i BATTERY_PERC=$BATTERY_NOW*100/$BATTERY_FULL
 
-# Icons
-ICON=''
-declare -i SELECTED_ICON=${BATTERY_PERC}/20-1
+# Battery file prefix
+cat /sys/class/power_supply/*/energy_full > '/dev/null' 2> '/dev/null' && \
+	PREFIX='energy'
 
-# Output
-echo "   ${ICON:${SELECTED_ICON}:1} ${BATTERY_PERC}% "
+cat /sys/class/power_supply/*/charge_full > '/dev/null' 2> '/dev/null' && \
+	PREFIX='charge'
 
-# Unset
-unset BATTERY_PERC BATTERY_FULL BATTERY_NOW ICON SELECTED_ICON
+
+# Print battery information if there is any
+if [ "$PREFIX" != '' ]; then
+	# Calculations
+	declare -i BATTERY_FULL=$(cat /sys/class/power_supply/BAT*/${PREFIX}_full | sed -n '1p')
+	declare -i BATTERY_NOW=$(cat /sys/class/power_supply/BAT*/${PREFIX}_now | sed -n '1p')
+	declare -i BATTERY_PERC=$BATTERY_NOW*100/$BATTERY_FULL
+
+	# Icons
+	ICON=''
+	declare -i SELECTED_ICON=${BATTERY_PERC}/20-1
+
+	# Output
+	echo "   ${ICON:${SELECTED_ICON}:1} ${BATTERY_PERC}% "
+fi
